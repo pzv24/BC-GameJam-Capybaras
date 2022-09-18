ddfc8ed9850e7d0e21a68bcdab7000ae07f73817
karlaem <kaarla.e.mg@gmail.com>
2022-09-17 19:38:18 -0700

    Merge

--- Assets/Scripts/BallLauncher.cs
+++ Assets/Scripts/BallLauncher.cs
@@ -10,20 +10,13 @@ public class BallLauncher : MonoBehaviour
 
     [Header("Arrow Data")]
     [SerializeField] private float _turnSpeed = 5f;
-    [SerializeField] private float halfAngleRange = 70;
-    [SerializeField] private float _forceBarCharge = 0.05f;
-    [SerializeField] private float _forceMultiplier = 100;
+    [SerializeField] private float halfAngleRange = 60;
 
     public Camera mainCamera;
     private Vector3 _mouseWorldLocation;
     private Vector2 _mousePosition;
     private Rigidbody _rb;
-    private Vector3 _rotationDifferential;
-    private float _forceCoef;
-    private float _incrementInterval = 0.1f;
-    private bool _isChargeing;
-
-    Quaternion targetRotation;
+    private Vector3 _currentRotation;
     private Quaternion currentRotation => transform.rotation;
 
 
@@ -33,71 +26,12 @@ public class BallLauncher : MonoBehaviour
     }
     public void OnRotateLeft(InputValue value)
     {
-        _rotationDifferential = new Vector3(0, -value.Get<float>() * _turnSpeed, 0);
+        Debug.Log("A"); 
     }
 
-    public void OnRotateRight(InputValue value)
+    private void FixedUpdate()
     {
-         _rotationDifferential = new Vector3(0, value.Get<float>() * _turnSpeed, 0);
+        transform.Rotate(_currentRotation);
     }
 
-    IEnumerator ChargeMeter()
-    {
-        while (true)
-        {
-            bool goingUp = true;
-            while (goingUp)
-            {
-                goingUp = CountUp();
-                if (_forceCoef > 1f) { goingUp = false; }
-                Debug.Log(_forceCoef);
-                yield return new WaitForSeconds(_incrementInterval);
-            }
-            while (!goingUp)
-            {
-                goingUp = CountDown();
-                if (_forceCoef < 0f) { goingUp = true; }
-                Debug.Log(_forceCoef);
-                yield return new WaitForSeconds(_incrementInterval);
-            }
-        }
-    }
-
-    private bool CountUp()
-    {
-        _forceCoef += _forceBarCharge;
-        return true;
-    }
-    private bool CountDown()
-    {
-        _forceCoef -= _forceBarCharge;
-        return false;
-    }
-
-    private void LaunchBall()
-    {
-        GameObject ball = Instantiate(_ball, transform.position, transform.rotation);
-        Rigidbody ballRB = ball.GetComponent<Rigidbody>();
-        ballRB.AddForce(transform.forward *_forceBarCharge * _forceMultiplier);
-
-    }
-
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Charging");
-            StartCoroutine(ChargeMeter());
-        }
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            StopAllCoroutines();
-            LaunchBall();
-        }
-
-        transform.Rotate(_rotationDifferential * Time.deltaTime);
-        Debug.Log(_rotationDifferential);
-    }
 }
