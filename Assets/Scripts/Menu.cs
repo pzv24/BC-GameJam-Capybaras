fa30f89880dd5e1018670ef8f09a9e6906b3a559
Santiago Lagunes F <gd67santiago@vfs.com>
2022-09-17 19:35:47 -0700

    Main Menu with credits

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -10,4 +10,9 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
 }
